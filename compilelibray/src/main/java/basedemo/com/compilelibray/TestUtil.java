de78a38a28dfc73b1fd53b6a1203db1c7067bafb
yibao.yang <yibao.yang@lingware.cn>
2017-10-30 15:05:28 +0800

    compile库初始化测试

---
+++ compilelibray/src/main/java/basedemo/com/compilelibray/TestUtil.java
@@ -0,0 +1,11 @@
+package basedemo.com.compilelibray;
+
+/**
+ * Created by yibao on 2017/10/30.
+ */
+
+public class TestUtil {
+    public static int addInt(int a, int b) {
+        return a + b;
+    }
+}
